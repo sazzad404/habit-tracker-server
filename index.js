500b306ff7d5a0a7e3049a8488e2276b382a0792
Md Sazzad Hasan <sazzadhasan313@gmail.com>
2025-11-12 02:41:02 +0600

    all finished

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ async function run() {
       return streak;
     }
 
-    await client.db("admin").command({ ping: 1 });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
